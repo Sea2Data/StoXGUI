7e6d18da428d65ae0c8b3eada28d226fbdd89e40
Hassen <esmaelmh@hi.no>
2019-05-03 11:36:17 +0200

    retrieve from springboot

--- frontend/src/app/data/data.service.ts
+++ frontend/src/app/data/data.service.ts
@@ -1,27 +1,21 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  stations : any;
-
   constructor(private httpClient: HttpClient) { }
 
-  getstations() {
+  getstations(): Promise<string> {
     // return this.httpClient.get("http://localhost:8080/geojson", {responseType: 'json'});
-    this.httpClient.get("http://localhost:8080/geojson", {responseType: 'text'}).subscribe(response => {this.stations = response.toString});
-  }
-
-  getsts() : string {
-    console.log("her");
-    this.getstations();
-    console.log(this.stations);
-    return this.stations;
+    return this.httpClient.get("http://localhost:8080/geojson", { responseType: 'text' })
+      .toPromise()
+      .then(res=>{
+        return res;
+      });
   }
 
 }
--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -18,17 +18,17 @@ import { register } from 'ol/proj/proj4';
 import * as proj4x from 'proj4';
 
 import { add as addProjection } from 'ol/proj/projection';
-import { Fill, Stroke, Style, RegularShape } from 'ol/style'; 
+import { Fill, Stroke, Style, RegularShape } from 'ol/style';
 import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 import { click, singleClick, shiftKeyOnly } from 'ol/events/condition';
 import Select from 'ol/interaction/Select';
 import { ResizedEvent } from 'angular-resize-event';
-import {defaults as defaultControls} from 'ol/control';
+import { defaults as defaultControls } from 'ol/control';
 import MousePosition from 'ol/control/MousePosition';
-import {createStringXY} from 'ol/coordinate';
+import { createStringXY } from 'ol/coordinate';
 
-import {DataService} from '../data/data.service';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-map',
@@ -44,8 +44,9 @@ export class MapComponent implements OnInit {
   // layer: OlTileLayer;
   vector: VectorLayer;
   view: OlView;
+  stations: any;
 
-  constructor(private dataService : DataService) {}
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
     /*this.source = new OlXYZ({
@@ -158,17 +159,23 @@ export class MapComponent implements OnInit {
         angle: Math.PI / 4
       })
     });
-    this.map.addLayer(new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        features: (new GeoJSON).readFeatures(JSON.parse(this.dataService.getsts()), {
-				defaultDataProjection: 'EPSG:4326',
-				featureProjection: proj
-			  })
-      }),
-      style: s,
-      selectable: true 
-    }));
+    this.dataService.getstations().then(
+      (st: any) => {
+        this.stations = st;
+        console.log(this.stations);
+
+        this.map.addLayer(new VectorLayer({
+          source: new VectorSource({
+            format: new GeoJSON(),
+            features: (new GeoJSON).readFeatures(JSON.parse(st), {
+              defaultDataProjection: 'EPSG:4326',
+              featureProjection: proj
+            })
+          }),
+          style: s,
+          selectable: true
+        }));
+      });
     //this.map.on('click', this.onClick());
     var selectClick = new Select({
       //condition: (mapBrowserEvent) => {
